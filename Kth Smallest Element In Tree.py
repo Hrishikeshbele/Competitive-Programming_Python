03cf27a08e4b1ca8e3f3ff08eedb4c9c249d7f81
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-06-30 17:37:21 +0530

    Create Kth Smallest Element In Tree.py

---
+++ Kth Smallest Element In Tree.py
@@ -0,0 +1,29 @@
+'''
+Given a binary search tree, write a function to find the kth smallest element in the tree.
+'''
+# Definition for a  binary tree node
+# class TreeNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.left = None
+#        self.right = None
+
+class Solution:
+    # @param A : root node of tree
+    # @param B : integer
+    # @return an integer
+    def kthsmallest(self,root, k):
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            #decrease 1 for every greater smallest elm
+            k -= 1
+            #if k==0 means we arrived at our kth smallest number
+            if k == 0:
+                return root.val
+            root = root.right
+        
+            
