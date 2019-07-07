5e1d678b0662ea1f3bdfba90f99c190dc4bb4f23
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-07-07 14:49:18 +0530

    Update Assign Mice to Holes.py

--- Assign Mice to Holes.py
+++ Assign Mice to Holes.py
@@ -5,6 +5,13 @@ A mouse can stay at his position, move one step right from x to x + 1, or move o
 Assign mice to holes so that the time when the last mouse gets inside a hole is minimized.
 '''
 #basic idea here is to sort the both arrays so that we get minimum distance for each mice
+'''
+Proof of correctness:
+Let i1 < i2 be the positions of two mice and let j1 < j2 be the positions of two holes. It suffices to show via case analysis that
+
+max(|i1-j1|, |i2-j2|) <= max(|i1-j2|, |i2-j1|), 
+   where '|a - b|' represent absolute value of (a - b)
+'''
 #solution1
 def mice(self, A, B):
         A.sort()
