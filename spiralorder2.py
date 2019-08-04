0dbeee13f2f4c5a9de520b06e7a99caaf7ff31b8
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-08-04 14:38:38 +0530

    Create spiralorder2.py

---
+++ spiralorder2.py
@@ -0,0 +1,21 @@
+'''
+Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+
+Example:
+
+Given n = 3,
+
+You should return the following matrix:
+
+[
+  [ 1, 2, 3 ],
+  [ 8, 9, 4 ],
+  [ 7, 6, 5 ]
+]
+'''
+def generateMatrix(self, n):
+    A, lo = [], n*n+1
+    while lo > 1:
+        lo, hi = lo - len(A), lo
+        A = [range(lo, hi)] + zip(*A[::-1])
+    return A
