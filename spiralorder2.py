fbd1712fb870deaf1d96d9d7b226aa10575f3b51
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-08-04 17:15:47 +0530

    Update spiralorder2.py

--- spiralorder2.py
+++ spiralorder2.py
@@ -19,3 +19,21 @@ def generateMatrix(self, n):
         lo, hi = lo - len(A), lo
         A = [range(lo, hi)] + zip(*A[::-1])
     return A
+#SOLUTION 2
+def generateMatrix(n):
+    
+    result = [[0 for i in range(n)] for j in range(n)]
+    coord = [[(i,j) for j in range(n)] for i in range(n)]
+    
+    count = 1
+    
+    while coord:
+        for x, y in coord.pop(0):
+            result[x][y] = count
+            count += 1
+            print(result)
+        coord = zip(*coord)[::-1]
+
+    return result
+  
+  
