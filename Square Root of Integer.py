9604ab2b965ea56fd5837ccb1bad17af233be318
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-08-08 12:40:59 +0530

    Update Square Root of Integer.py

--- Square Root of Integer.py
+++ Square Root of Integer.py
@@ -11,11 +11,13 @@ class Solution:
         e=A
         while s<=e:
             mid=(s+e)/2
+            # Checking if mid = floor(sqrt(A))
             if mid*mid<=A and (mid+1)*(mid+1)>A:
                 return mid
             elif mid*mid>A:
                 e=mid-1
             elif mid*mid<A:
                 s=mid+1
+        #if given 0 .that is only way we will get here
         return 0
                 
