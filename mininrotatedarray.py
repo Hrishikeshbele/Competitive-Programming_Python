3d0a7da881f3cffd616dca37c58bd13132b2ab62
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-08-11 12:20:03 +0530

    Update mininrotatedarray.py

--- mininrotatedarray.py
+++ mininrotatedarray.py
@@ -5,15 +5,19 @@ Suppose a sorted array A is rotated at some pivot unknown to you beforehand.
 
 Find the minimum element.
 '''
+'''
+ans: if we observe the rotated array we can see that since we use round up for mid, and left < right , right would never be the same as mid
+      Therefore, we compare mid with right, since they will never be the same from.if nums[mid] < nums[right] we see that second half(arr[mid:])
+      is in sorted order and min lies in first half similarly if nums[mid]>nums[right] we can see that min lies in first half arr[:mid+1] is sorted 
+      and min lies in second half so we set l to mid+1 and when we end while loop l=r=min elm
+  '''
 def findMin(A):
-        left = 0
-        right = len(A)-1
-        while left < right:
-            mid = (left+right)/2
-            
-            if A[left] < A[right] or A[mid] < A[left]:
-                right = mid
+        l,r=0,len(arr)-1
+        while l<r:
+            mid=(l+r)//2
+            if arr[mid]>arr[r]:
+                l=mid+1    
             else:
-                left = mid+1
-        
-        return A[left]
+                r=mid
+                
+        return arr[l]
