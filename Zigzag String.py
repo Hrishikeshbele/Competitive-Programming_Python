b74b0713aab71d5840a9e151fc92a8c71fc93868
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-08-14 18:56:23 +0530

    Update Zigzag String.py

--- Zigzag String.py
+++ Zigzag String.py
@@ -37,6 +37,30 @@ class Solution:
                 row += 1
             else:
                 row -= 1
+                
+  #second solution
+
+def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        L = [''] * numRows
+        index, step = 0, 1
+
+        for x in s:
+            L[index] += x
+            if index == 0:
+                step = 1
+            elif index == numRows -1:
+                step = -1
+            index += step
+
+        return ''.join(L)
   
     # Print concatenation 
     # of all rows 
