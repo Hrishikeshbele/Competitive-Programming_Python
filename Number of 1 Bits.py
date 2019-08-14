cbca999cdab1b3543aeadb91bb105e1d7e1b5836
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-08-14 20:47:23 +0530

    Update Number of 1 Bits.py

--- Number of 1 Bits.py
+++ Number of 1 Bits.py
@@ -25,6 +25,16 @@ class Solution:
             A=A/2
         return count
 
-        
+    
+ #bits solution
+#remeber that A-1 will unset last set bit and unset following bits then by taking and we are clearing that following set bits
+#so A&=A-1 will clear last set bit from repretn we count how many bits we cleared that is no of 1's in it's repretn
+def numSetBits(self, A):
+        count = 0
+        while A > 0:
+            A =A&A - 1
+            count += 1
+        return count
+
 
         
