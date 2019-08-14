4494749a5cc8377b1a6149e008a8686c980d9041
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-08-14 21:27:52 +0530

    Update Single Number.py

--- Single Number.py
+++ Single Number.py
@@ -13,3 +13,26 @@ class Solution:
             if value == 1:
                 return key
 	    
+#solution using xor operator:
+'''
+#xor operator will work as follow:
+res = 7 ^ 3 ^ 5 ^ 4 ^ 5 ^ 3 ^ 4
+
+Since XOR is associative and commutative, above 
+expression can be written as:
+res = 7 ^ (3 ^ 3) ^ (4 ^ 4) ^ (5 ^ 5)  
+    = 7 ^ 0 ^ 0 ^ 0
+    = 7 ^ 0
+    = 7 
+ '''
+
+def singleNumber(self, A):
+        x = 0
+        
+        for n in A:
+            x = x ^ n   
+        return x
+#or
+def singleNumber(self, A):
+	#reduce mape's given function to every elm of iterable.
+        return reduce(lambda x,y : x^y, A)
