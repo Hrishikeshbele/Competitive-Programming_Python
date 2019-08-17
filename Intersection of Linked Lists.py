938a909ec5d30db381fe9cf09fd81c43061ea1cb
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-08-17 13:01:39 +0530

    Update Intersection of Linked Lists.py

--- Intersection of Linked Lists.py
+++ Intersection of Linked Lists.py
@@ -42,6 +42,29 @@ class Solution:
             
         
         
+        
+   #second solution
+'''
+the idea is if you switch head, the possible difference between length would be countered. 
+ On the second traversal, they either hit or miss. 
+ if they meet, pa or pb would be the node we are looking for, 
+ if they didn't meet, they will hit the end at the same iteration, pa == pb == None, return either one of them is the same,None
+'''
+
+def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        pa,pb=headA,headB
+         # if either pointer hits the end, switch head of that pointer and continue the traversal, 
+          # if not hit the end, just move on to next
+        while pa is not pb:
+            pa= headB if pa is None else pa.next
+            pb= headA if pb is None else pb.next
+        # only 2 ways to get out of the loop, they meet or the both hit the end=None
+        return pa
+        
             
         
     
