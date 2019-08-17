c1b7b9b61ec337e1f26e243bccd58fcd79ebe32f
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-08-17 17:00:37 +0530

    Update linkedlistbasics.py

--- linkedlistbasics.py
+++ linkedlistbasics.py
@@ -8,3 +8,10 @@ def lenofll(head)
       len+=1
       head=head.next
   return len
+#finding middle of linked list using 2 pointer technique
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        #return slow:- slow points to mid elm in odd len case or first elm of second half in even len case.
+         
