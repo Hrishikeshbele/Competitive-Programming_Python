f2dd82053584b3c02d71c84dacc49153f6cf409a
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-08-24 17:24:51 +0530

    Update traversalsinBT.py

--- traversalsinBT.py
+++ traversalsinBT.py
@@ -3,6 +3,7 @@ how to perform different transversal in BT
 '''
 def preorder(root):
   return [root.val] + preorder(root.left) + preorder(root.right) if root else []
+# go to the left and run the function again,when there's no left, add value to the list.
 def inorder(root):
   return  inorder(root.left) + [root.val] + inorder(root.right) if root else []
 def postorder(root):
