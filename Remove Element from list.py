7e919a217cfdf3ef47af1d849bf2c48245435fd2
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2020-03-12 15:03:24 +0530

    Create Remove Element from list.py

---
+++ Remove Element from list.py
@@ -0,0 +1,30 @@
+'''
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example 1:
+
+Given nums = [3,2,2,3], val = 3,
+
+Your function should return length = 2, with the first two elements of nums being 2.
+
+solution idea: we maintain pointer to next to list without val and if num is not equl to val we assign it to pointer and increase the pointer
+'''
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        point=0
+        for i in nums:
+            if i!=val:
+                nums[point]=i
+                point+=1
+            
+        return point
+                
