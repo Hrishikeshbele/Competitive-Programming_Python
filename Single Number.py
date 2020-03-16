ba16cb0d51bd968b19384f55251012be8ff0aa6b
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2020-03-16 19:20:36 +0530

    Update Single Number.py

--- Single Number.py
+++ Single Number.py
@@ -4,6 +4,8 @@ Example :
 Input : [1 2 2 3 1]
 Output : 3
 '''
+#solution_1
+
 from collections import Counter
 class Solution:
 	# @param A : tuple of integers
@@ -12,7 +14,8 @@ class Solution:
 	    for key,value in Counter(list(A)).items():
             if value == 1:
                 return key
-	    
+
+#solution_2
 #solution using xor operator:
 '''
 #xor operator will work as follow:
@@ -28,11 +31,38 @@ res = 7 ^ (3 ^ 3) ^ (4 ^ 4) ^ (5 ^ 5)
 
 def singleNumber(self, A):
         x = 0
-        
+        #in every iteration will take xor of curr elm with all prev elm in this way if we have repeating elm them their xor will be 
+	# zero and only elm whose count is 1 will remain.
         for n in A:
             x = x ^ n   
         return x
 #or
+#solution_3
+
 def singleNumber(self, A):
 	#reduce mape's given function to every elm of iterable.
         return reduce(lambda x,y : x^y, A)
+
+#solution_4
+
+#solution using dict.we increment corresping elm(key) in dict every time we incounter that elm or add to dict if not present and then
+# return the key value where value is 1.
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dicn={}
+        for i in nums:
+            if i not in dicn:
+                dicn[i]=1
+            else:
+                dicn[i]+=1
+        for key,val in dicn.items():
+            if val==1:
+                return key
+        
+            
+        
