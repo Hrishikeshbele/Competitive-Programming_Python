bce698401bcfe58d5bb3ba3b463d58cf56ffa97b
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2020-04-03 15:58:01 +0530

    Update Majority Element.py

--- Majority Element.py
+++ Majority Element.py
@@ -34,9 +34,11 @@ class Solution(object):
         for num in nums:
             if num not in dic:
                 dic[num] = 1
+            else:
+                dic[num] += 1
             if dic[num] > len(nums)//2:
                 return num
-            else:
-                dic[num] += 1 
+
+        
 
         
