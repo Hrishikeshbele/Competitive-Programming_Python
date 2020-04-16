f2fa7e1b88060f5c2907b70ccd045cee77a3c83f
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2020-04-16 16:41:36 +0530

    Update shor_url_class.py

--- OOPS/shor_url_class.py
+++ OOPS/shor_url_class.py
@@ -8,3 +8,46 @@ Key = short-URL Value = Long-URL
 Key: random variable length alphabetic suffix [tinyurl.com/tgwxyc]
 
 '''
+
+# Class variables shared by all objects
+
+# Class: group all variables/attributes and functions/methods into a single logical unit
+
+class ShortURL1:
+    
+    URLPrefix = "https://www.shortURL.com/"; # class variable 
+    
+    def __init__(self): # constructor
+        self.d=dict();
+
+    # given a long URL, get a short URL
+    def getShortURL(self, longURL): 
+        # length = random value in 6-10
+        l = random.randint(6,10);
+        
+
+        # generate random characters into a string of length l
+        chars = string.ascii_lowercase
+        shortURL = ''.join(random.choice(chars) for i in range(l))
+        
+
+        # check if this string is already present in dict d
+        if shortURL in self.d:
+            return getShortURL(longURL);
+        else:
+            self.d[shortURL] = longURL;
+
+        
+        r = self.URLPrefix + shortURL
+        return r;
+
+    def getLongURL(self, shortURL):
+        
+        # extarct key from URL https://www.shortURL.com/mxzmuis ---> mxzmuis
+        k = shortURL[25:];
+
+        if k in self.d:
+            return self.d[k];
+        else:
+            return None;
+
