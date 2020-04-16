461c8c873d89f3d5b6826fd11ac19a22cd49c463
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2020-04-16 15:42:56 +0530

    Create shorted_url_class.py

---
+++ OOPS/shorted_url_class.py
@@ -0,0 +1,10 @@
+# OOP: Easier to design, reuse components and build large software
+# OOP for ML & DS roles: Should know the basics to use various modules and extend them when needed.
+'''
+Q. How would you build a TinyURL service?
+
+approach: 
+Key = short-URL Value = Long-URL
+Key: random variable length alphabetic suffix [tinyurl.com/tgwxyc]
+
+'''
