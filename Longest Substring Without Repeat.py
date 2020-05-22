739b80f1ca6a46d706ac8e5344d76bd6feed4a5a
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2020-05-22 14:11:11 +0530

    Update Longest Substring Without Repeat.py

--- Longest Substring Without Repeat.py
+++ Longest Substring Without Repeat.py
@@ -25,3 +25,26 @@ class Solution:
     # non-repeated substring
         return max(res, len(s)-start)
         
+#solution-2
+# idea is to store the each chart and its prev seen position as dict and if we encounter a repeated char we start with new string and for each string and each increasing char we keep track of max length of string we encounterd
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dicts={}
+        #start var to keep track of start of new substring after repeating charecters
+        start=max_len=0
+        for i,val in enumerate(s):
+            if val in dicts and dicts[val]>=start :
+                # we move one char forward from last seen postion of char
+                start=dicts[val]+1
+          
+            else:
+                max_len=max(max_len,i-start+1)	
+            #storing last seen pos of char in dict 
+            dicts[val]=i
+        return max_len
+  
+        
