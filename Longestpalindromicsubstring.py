5c9a6118ae6c46cd5156f15ec7a80b2034ec0f94
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2020-05-22 20:05:07 +0530

    Update Longestpalindromicsubstring.py

--- Longestpalindromicsubstring.py
+++ Longestpalindromicsubstring.py
@@ -34,3 +34,26 @@ def longestPalindrome(s):
                     start=i
                     maxl=k
     return s[start:start+maxl]
+
+#solution2
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res=''
+        for i in range(len(s)):
+            # (s,i,i) for odd case, like "aba"  
+            # (s,i,i+1) for even case, like "abba"
+            res=max(self.helper(s,i,i),self.helper(s,i,i+1),res,key=len)
+        return res
+    # get the longest palindrome, l, r are the middle indexes from inner to outer
+    def helper(self,s,l,r):
+        while l>=0 and r<len(s) and s[l]==s[r]:
+            l-=1
+            r+=1
+        return s[l+1:r]
+        
+        
