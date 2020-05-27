fe79f1846da2a11baa9c656b4b22b4562b7e6dc5
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-05-27 13:30:57 +0530

    Update Swap List Nodes in pairs.py

--- Swap List Nodes in pairs.py
+++ Swap List Nodes in pairs.py
@@ -22,3 +22,33 @@ class Solution:
             curr=curr.next.next
          
         return A
+
+    
+#solution 2
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        #dummy is assigned to prev and prev to empty node
+        dummy=prev=ListNode(0)
+        #pointing prev to head
+        prev.next=head
+        while prev.next and prev.next.next:
+            a=prev.next
+            b=prev.next.next
+            c = prev.next.next.next
+            prev.next=b
+            prev.next.next=a
+            prev.next.next.next = c
+            #prev will have its val that of a and next pointer to c since we need pointer to             # next elm after these 2 elms
+            prev=prev.next.next
+        return dummy.next
+            
