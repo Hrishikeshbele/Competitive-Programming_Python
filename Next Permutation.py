4e6eb19e542cc93090b5390dd33e492aa6ffefcf
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-06-25 13:12:59 +0530

    Update Next Permutation.py

--- Next Permutation.py
+++ Next Permutation.py
@@ -11,7 +11,8 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 3,2,1 → 1,2,3
 1,1,5 → 1,5,1
 
-idea: we start from 
+idea: we start from back and find the i where decreasing sequence will start from i=1. then we find the elm at j which is next greater elm to elm at i and replace it with i and 
+then we reverse the array from index i+1.
 '''
 
 
@@ -21,22 +22,21 @@ def nextpermutation(nums):
         return 
     #getting last elm index to start backtracking with
     i=len(nums)-2
-
+    # finding i+1 from where decreasing sequence starts 
     while i>=0 and nums[i]>=nums[i+1]:
         i-=1
-    
+    # if we come out of while loop unnaturally then and the elm to be replaced with elm just outside decreasing sequence
     if i != -1:
         j=i+1
         while j<(len(nums)) and nums[j]>nums[i]:
             j+=1
-            
-        
+        #substracting last unwanted increase 
         j-=1
+        #exchanging elm at j and i indexes
         nums[i],nums[j]=nums[j],nums[i]
     return nums[:i+1]+nums[i+1:][::-1]
 
-nums=[1,3,2]
-print(nextpermutation(nums))
+
 
 
     
