ecf5efa02912039cd17cfe47c511af467efa1c69
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-06-26 16:18:38 +0530

    Update Find First and Last Position of Element .py

--- Find First and Last Position of Element .py
+++ Find First and Last Position of Element .py
@@ -10,6 +10,8 @@ Example 1:
 Input: nums = [5,7,7,8,8,10], target = 8
 Output: [3,4]
 
+idea here is first we find the target element using binary search and then iterate left and right of it to find max and min index of target elm.
+
 '''
 
 class Solution(object):
@@ -24,27 +26,22 @@ class Solution(object):
         # corner cases 
         if not nums:
             return ans
-        if len(nums)<2 and nums[0]!=target:
-            return ans
-        if len(nums)<2 and nums[0]==target:
-            return [0,0]
-        
         
         while l<=r:
             mid=(l+r)//2
             if nums[mid]==target:
                 ans=[mid,mid]
+                # left to target search search 
                 if nums[mid-1]==target:
                     j=mid-1
                     while nums[j]==target and j >=0:
                         ans[0]=j
                         j-=1
-                        
+                #right to target search 
                 if mid+1<len(nums) and nums[mid+1]==target:
-                    
                     if nums[mid+1]==target:
                         k=mid+1
-                        while   k<len(nums) and nums[k]==target:
+                        while k<len(nums) and nums[k]==target:
                             ans[1]=k
                             k+=1
                             
