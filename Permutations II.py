71888ab3ee2de1b97be7cc812c82a8d4f67fbf05
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-06-30 19:04:07 +0530

    Update Permutations II.py

--- Permutations II.py
+++ Permutations II.py
@@ -12,3 +12,23 @@ Output:
 ]
 
 '''
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res=[]
+        nums.sort()
+        self.helper(nums,[],res)
+        return (res)
+    
+    def helper(self,nums,path,res):
+        if not nums :
+            res.append(path)
+            return 
+        for i in range(len(nums)):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            self.helper(nums[:i]+nums[i+1:],path+[nums[i]],res)
