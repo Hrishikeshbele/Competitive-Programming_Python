8f634b06ee363393eb17358a6b926a9ceb6f1894
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-07-01 10:51:07 +0530

    Update Subsets II_dfs.py

--- Subsets II_dfs.py
+++ Subsets II_dfs.py
@@ -37,6 +37,7 @@ dfs(nums = [1,2,2], path = [], res = [])
 
 if 2 consecative elements are same then we just do it for one of 2 elements because we will be doing duplicate work if we do it for  2 elms.The inner loop will process the first "1," 
 then skip the next "1's". for example nums[1,1] and path=[2] then if we proceed with this pair will will get 2 same subsets i.e. [[2,1] ,[2,1]] .
+why sorting?: because the input might contain duplicates, after sorting, duplicated number will be next to each other, this way this duplication check nums[i] == nums[i-1] will work.
 '''
 
 class Solution(object):
