0102e590b77c9b87cf2efc6f5e969a81cc50f34e
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-09-09 22:26:14 +0530

    Update Gray Code.py

--- Gray Code.py
+++ Gray Code.py
@@ -3,7 +3,11 @@ he gray code is a binary numeral system where two successive values differ in on
 
 Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
 
+Approach:
 For example, given n = 2, return [0,1,3,2]
+For n=2: 00 01 11 10, Notice that the second half (11 and 10) are mirror of the first half (0 1) with additional 1 before it (10 11).
+Now we have (00 01 11 10), in order to do n=3 we need to do the same. The 4 elements of n=2 will be our first half, to do the second half we mirror them to get (10 11 01 00) 
+and add additional 1 before it (110 111 101 100). We get: For n=3: 000 001 011 010 110 111 101 100
 '''
 
 class Solution:
@@ -40,3 +44,20 @@ def grayCode(A):
         res = res + temp
     return res    
 
+## solution 3
+
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        def helper(n):
+            if n==0:
+                return ['0']
+            if n==1:
+                return ['0','1']
+            res=helper(n-1)
+            return ['0'+i for i in res]+['1'+i for i in res[::-1]]
+        
+        return [int(j,2) for j in helper(n)]
