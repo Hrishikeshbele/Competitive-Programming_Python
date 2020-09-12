64f3ac44ee501b7a52536eb9b86e27c0f4748b1b
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-09-12 19:34:06 +0530

    Create Construct Binary Tree from Preorder and Inorder Traversal.py

---
+++ Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,48 @@
+'''
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+Return the following binary tree:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+   
+steps to build tree:
+
+Use the first element of preorder, the 1, as root.
+Search it in inorder.
+Split inorder by it, here into [4, 2, 5] and [6, 3].
+Split the rest of preorder into two parts as large as the inorder parts, here into [2, 4, 5] and [3, 6].
+Use preorder = [2, 4, 5] and inorder = [4, 2, 5] to add the left subtree.
+Use preorder =[3, 6]andinorder = [6, 3] to add the right subtree.
+
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if inorder:
+            ind=inorder.index(preorder.pop(0))
+            node=TreeNode(inorder[ind])
+            node.left=self.buildTree(preorder,inorder[:ind])
+            node.right=self.buildTree(preorder,inorder[ind+1:])
+                                    
+            return node
