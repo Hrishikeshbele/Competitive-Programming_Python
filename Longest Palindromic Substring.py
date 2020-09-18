74c261840cd6750cbc647f94909f097b81bcf57a
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-09-18 16:49:54 +0530

    Create Longest Palindromic Substring.py

---
+++ Longest Palindromic Substring.py
@@ -0,0 +1,50 @@
+'''
+Given a string S, find the longest palindromic substring in S.
+
+Substring of string S:
+
+S[i...j] where 0 <= i <= j < len(S)
+
+Palindrome string:
+
+A string which reads the same backwards. More formally, S is palindrome if reverse(S) = S.
+
+Incase of conflict, return the substring which occurs first ( with the least starting index ).
+
+Example :
+
+Input : "aaaabaaa"
+Output : "aaabaaa"
+
+
+at each elm we go left and right and see how much longest substring is possible.if that string has length more that max len string until now. we update the max str.
+
+'''
+
+class Solution:
+    # @param A : string
+    # @return a strings
+    def longestPalindrome(self, a):
+        def helper(l,r):
+            while l>=0 and r<len(a) and a[l]==a[r]:
+                l-=1
+                r+=1
+                
+            return a[l+1:r] #return 1 step back from front and back since curr pair doesn't
+                            # satisfy palindrome prop
+                
+        ans=''
+        for i in range(len(a)):
+            # longest string with odd no of chars
+            temp=helper(i,i)
+            if len(temp)>len(ans):
+                ans=temp
+            #if longest string cont even no of chars
+            temp=helper(i,i+1)
+            if len(temp)>len(ans):
+                ans=temp
+       
+        return ans
+        
+            
+            
