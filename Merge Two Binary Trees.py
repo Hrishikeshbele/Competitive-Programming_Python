e1fc5b4d50e354aa156095607cb53fb00a04a476
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-09-27 23:59:37 +0530

    Create Merge Two Binary Trees.py

---
+++ Merge Two Binary Trees.py
@@ -0,0 +1,46 @@
+'''
+Problem Description
+
+Given two Binary Trees A and B, you need to merge them in a single binary tree.
+
+The merge rule is that if two nodes overlap, then sum of node values is the new value of the merged node.
+
+Otherwise, the non-null node will be used as the node of new tree.
+
+
+Example 1:
+
+Input: 
+	Tree 1                     Tree 2                  
+          1                         2                             
+         / \                       / \                            
+        3   2                     1   3                        
+       /                           \   \                      
+      5                             4   7                  
+Output: 
+Merged tree:
+	     3
+	    / \
+	   4   5
+	  / \   \ 
+	 5   4   7
+   
+   
+   
+  idea here is we will add values from each trees if values is present in both trees otherwise we will just use whichever value is present
+  
+  '''
+  
+  class Solution:
+    # @param A : root node of tree
+    # @param B : root node of tree
+    # @return the root node in the tree
+    def solve(self, A, B):
+        new=TreeNode(None)
+        if A and B:
+            new.val=A.val+B.val
+            new.left=self.solve(A.left,B.left)
+            new.right=self.solve(A.right,B.right)
+            return new
+        else:
+            return A or B
