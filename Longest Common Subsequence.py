04a8845dab8ae14fc54d7aae519285f05546a74a
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-09-29 19:00:22 +0530

    Create Longest Common Subsequence.py

---
+++ Longest Common Subsequence.py
@@ -0,0 +1,46 @@
+'''
+Longest Common Subsequence
+
+Problem Description
+
+Given two strings A and B. Find the longest common sequence ( A sequence which does not need to be contiguous), which is common in both the strings.
+
+You need to return the length of such longest common subsequence.
+Input 1:
+
+ A = "abbcdgf"
+ B = "bbadcgf"
+
+
+Example Output
+Output 1:
+
+ 5
+
+Example Explanation
+Explanation 1:
+
+ The longest common subsequence is "bbcgf", which has a length of 5
+ 
+ idea: to understand the solution approach watch the video by bak to back swe youtube channel. he explained solution approach this problem beatifully . 
+ 
+ '''
+ 
+ class Solution:
+    # @param A : string
+    # @param B : string
+    # @return an integer
+    def solve(self, A, B):
+        A=' '+A
+        B=' '+B
+        ans=[[0 for i in range(len(A))] for j in range(len(B))]
+        for i in range(1,len(A)):
+            for j in range(1,len(B)):
+                if not B[j] or not A[i]:
+                    ans[i][j]=0
+                elif A[i]==B[j]:
+                    ans[i][j]=ans[i-1][j-1]+1
+                elif A[i]!=B[j]:
+                    ans[i][j]=max(ans[i-1][j],ans[i][j-1])
+        return ans[-1][-1]
+        
