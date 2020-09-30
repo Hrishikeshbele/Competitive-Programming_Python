59c394ef7eae2b1d68a0e2d0b405b97d9b37fb16
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-09-30 13:37:50 +0530

    Update next greatest elment to right.py

--- next greatest elment to right.py
+++ next greatest elment to right.py
@@ -5,7 +5,8 @@ ex.[1,3,4,2]
 
 outpt: [3, 4, -1, -1]
 
-we use the concept of stack. we transverse from back of array and keep one stack to store right elms. for whole approach see the youtube video of aditya verma.
+we use the concept of stack. we transverse from back of array and keep one stack to store right elms.if current elm is less than last elm of stack we pop elms in stack until
+we get elm greater than curr in stack and we append that elm to ans if we don't get any such elm we append -1 to ans. for whole approach see the youtube video of aditya verma.
 
 '''
 def ngr(a):
