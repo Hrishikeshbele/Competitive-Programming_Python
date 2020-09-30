e79b35f005dca4bd382f9ba4a733a2d81b90e4bc
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-09-30 13:32:52 +0530

    Create next greatest elment to right.py

---
+++ next greatest elment to right.py
@@ -0,0 +1,27 @@
+'''
+given a array find the next greatest element of each element in array
+
+ex.[1,3,4,2]
+
+outpt: [3, 4, -1, -1]
+
+we use the concept of stack. we transverse from back of array and keep one stack to store right elms. for whole approach see the youtube video of aditya verma.
+
+'''
+def ngr(a):
+    stack=[]
+    ans=[]
+    for i in range(len(a)-1,-1,-1):
+        if not stack:
+            ans.append(-1)
+        elif stack and stack[-1]>a[i]:
+            ans.append(stack[-1])
+        elif stack and stack[-1]<a[i]:
+            while stack and stack[-1]<=a[i]:
+                stack.pop()
+            if stack:
+                ans.append(stack[-1])
+            elif not stack:
+                ans.append(-1)
+        stack.append(a[i])
+    return ans[::-1]
