a05f94f7c3e66630b387058c1997192c4ea04550
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-10-11 16:56:34 +0530

    Update Container with max water.py

--- Container with max water.py
+++ Container with max water.py
@@ -7,7 +7,13 @@ Note: You may not slant the container and n is at least 2.
 the idea here is to use 2 pointers and move the left pointer and right pointer respectively to search for the next higher height.If height[L] < height[R],
 move toward right(increment left pointer), else move left since Say height[0] < height[5],since area of (0, 4), (0, 3), (0, 2), (0, 1) will be smaller than 
 (0, 5) since height will be equal to height[0] and lenth would const decrease, so no need to try them.           
-
+ex.
+[3....<10 sticks>....5]` water between 3, 5 = 3*11 = 33.
+Argument: You have to move 3 because moving the index at 5 will certainly reduce the water level.
+        3...<10 sticks>....8,5 => Water is 3*10 = 30 which is lower than before.
+        i                  j
+        3...<10 sticks>....2,5 => Again lower in this case as compared to the maximal
+        
 '''
 
 class Solution(object):
