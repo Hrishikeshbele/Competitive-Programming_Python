530459155683652d58bd7e98a67413d1c0fcc8b8
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-10-11 15:55:50 +0530

    Update Container with max water.py

--- Container with max water.py
+++ Container with max water.py
@@ -1,12 +1,12 @@
 '''
-Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that 
+Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that 
 the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container
 contains the most water.
 Note: You may not slant the container and n is at least 2.
 
-idea here is use 2 pointers and  move the left pointer and right pointer respectively to search for the next higher height.If height[L] < height[R],
-move toward right, else move left since Say height[0] < height[5],since area of (0, 4), (0, 3), (0, 2), (0, 1) will be smaller than 
-(0, 5) since height will be equal to height[0] and lenght would const decrease,so no need to try them.           
+the idea here is to use 2 pointers and move the left pointer and right pointer respectively to search for the next higher height.If height[L] < height[R],
+move toward right(increment left pointer), else move left since Say height[0] < height[5],since area of (0, 4), (0, 3), (0, 2), (0, 1) will be smaller than 
+(0, 5) since height will be equal to height[0] and lenth would const decrease, so no need to try them.           
 
 '''
 
