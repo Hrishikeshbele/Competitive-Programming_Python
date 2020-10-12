f836ac1853d194552c670dc194f3100d65bb5dc4
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-10-12 13:22:10 +0530

    Update House Robber.py

--- House Robber.py
+++ House Robber.py
@@ -14,6 +14,15 @@ Based on the recursive formula:
 f(0) = nums[0]
 f(1) = max(num[0], num[1])
 f(k) = max( f(k-2) + nums[k], f(k-1) )
+
+A robber has 2 options: a) rob current house i; b) don't rob current house.
+If an option "a" is selected it means she can't rob previous i-1 house but can safely proceed to the one before previous i-2 and gets all cumulative loot that follows.
+If an option "b" is selected the robber gets all the possible loot from robbery of i-1 and all the following buildings.
+So it boils down to calculating what is more profitable:
+
+robbery of current house + loot from houses before the previous
+loot from the previous house robbery and any loot captured before that
+rob(i) = max( rob(i - 2) + currentHouseValue, rob(i - 1) )
 '''
 class Solution(object):
     def rob(self, nums):
