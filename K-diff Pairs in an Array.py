3137c0735ed8d2c616430308196dfb9e153b37a9
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-10-14 17:31:15 +0530

    Update K-diff Pairs in an Array.py

--- K-diff Pairs in an Array.py
+++ K-diff Pairs in an Array.py
@@ -33,3 +33,46 @@ class Solution:
                     pairs.add((num, otherNum) if num <= otherNum else (otherNum, num))
                     
         return len(pairs)
+
+# solution 2 (with 2 pointers techs)
+
+def findPairs(self, a, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        a.sort()
+        ans=set()
+        i,j=0,1
+	 """
+        Special Case: k = 0 # below code will include same elm 2 time as pair(when i==j) which we don't want
+        - Use the Python collections Counter to get the frequency
+          of each value in nums.
+        - Determine the number of zero differences by counting
+          the number of values in nums with frequency > 1.
+        """
+        if k == 0:
+            freqs = Counter(a)
+            return sum(freqs[k] > 1 for k in freqs)
+	 """
+        General Case: k > 0
+        - produce a sorted list of values
+        - Use two pointers (slow and fast) in a sliding window
+          protocol to find all pairs whose difference is k.
+          - If difference is smaller than k, advance fast pointer.
+          - If difference is larger than k, advance slow pointer.
+          - If difference equals k, advance both pointers and
+            increment a counter that is later returned from this
+            program.
+        """
+        while  j<len(a):
+            if abs(a[j]-a[i])==k:
+                ans.add((a[i],a[j]))
+                i+=1
+                j+=1
+            elif a[j]-a[i]<k:
+                j+=1
+            else:
+                i+=1
+        return len(ans)
