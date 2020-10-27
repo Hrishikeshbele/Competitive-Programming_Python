4a238d5a2385aa67c6cf3a1a1a9e74020178a70d
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-10-27 18:01:48 +0530

    Update N Unique Integers Sum  to Zero.py

--- N Unique Integers Sum  to Zero.py
+++ N Unique Integers Sum  to Zero.py
@@ -10,3 +10,35 @@ class Solution(object):
         :rtype: List[int]
         """
         return range(1-n,n,2)
+
+# solution2 
+'''
+Explanation:
+First, take a look at a few examples:
+
+n = 1: ans = [0]
+n = 2: ans = [-1,1]
+n = 3: ans = [-1,0,1]
+n = 4: ans = [-2,-1,1,2]
+n = 5: ans = [-2,-1,0,1,2]
+So, we should return an array where the values are symmetric.
+If n%2 is not equal to zero (n is odd), we append 0 to the answer list.
+So, we use a for loop; and each time we add -i and i to the answer list.
+
+'''
+
+class Solution(object):
+    def sumZero(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans=[]
+        if n%2==0:# n is even
+            for i in range(1,(n/2)+1): # we iterate n/2 +1 times
+                ans.extend([i,-i])
+        else: # n is odd
+            for i in range(1,(n//2)+1):
+                ans.extend([i,-i])
+            ans.append(0)
+        return ans
