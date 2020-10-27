ab73d4ba1060b671205f623fe8854544db4f248a
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-10-27 16:00:37 +0530

    Update Minimize the absolute difference.py

--- Minimize the absolute difference.py
+++ Minimize the absolute difference.py
@@ -35,4 +35,46 @@ class Solution:
                 b += 1
             else:
                 c += 1
-        return mini
+        return 
+   
+
+# solution 2 
+
+'''
+same approach but with largest element instead of smallest elm.In every step, the only possible way to decrease the difference is to decrease the maximum element out of 
+the three elements.So traverse to the next largest element in the array containing the maximum element for this step and update the answer variable.
+'''
+
+def solve(A, B, C): 
+  
+        # assigning the length -1 value 
+        # to each of three variables 
+        i = len(A) - 1
+        j = len(B) - 1
+        k = len(C) - 1
+  
+        # calculating min difference  
+        # from last index of lists 
+        min_diff = abs(max(A[i], B[j], C[k]) -
+        min(A[i], B[j], C[k])) 
+  
+        while i != -1 and j != -1 and k != -1: 
+            current_diff = abs(max(A[i], B[j],  
+            C[k]) - min(A[i], B[j], C[k])) 
+  
+            # checking condition 
+            if current_diff < min_diff: 
+                min_diff = current_diff 
+  
+            # calculating max term from list 
+            max_term = max(A[i], B[j], C[k]) 
+  
+            # Moving to smaller value in the 
+            # array with maximum out of three. 
+            if A[i] == max_term: 
+                i -= 1
+            elif B[j] == max_term: 
+                j -= 1
+            else: 
+                k -= 1
+        return min_diff 
