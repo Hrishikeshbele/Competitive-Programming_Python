64f252a3eb39dbbf8ac1edcdc023b06e47371904
Hrishikesh Bele <Hrishikeshbele@users.noreply.github.com>
2020-10-28 09:48:53 +0530

    Update Permutations of list.py

--- Permutations of list.py
+++ Permutations of list.py
@@ -44,3 +44,22 @@ def permutations(s):
         for j in perm:
             ans.append([a]+j)
     return ans
+
+# backtracking solution
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res=[]
+        self.helper(nums,[],res)
+        return res
+    
+    def helper(self,nums,path,res):
+        if not nums:
+            res.append(path)
+            return 
+        for i in range(len(nums)):
+            self.helper(nums[:i]+nums[i+1:],path+[nums[i]],res)
